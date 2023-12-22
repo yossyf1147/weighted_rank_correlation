5f10df047ce4a408631c6eea48b6cecc9111f5d5
Yoshi <yossyf1147@gmail.com>
2023-12-22 18:03:56 +0100

    make it ranking if its not integer(for fast calculation)

--- gamma_correlation/main.py
+++ gamma_correlation/main.py
@@ -22,7 +22,9 @@ def gamma_corr(ranking_a: Union[list, numpy.ndarray, np.ndarray], ranking_b: Uni
     """
     rankings = np.array([ranking_a, ranking_b])
     if np.array_equal(rankdata(rankings, axis=1, method="ordinal"), rankings):
+        print("before": rankings)
         rankings = rankdata(rankings, axis=1, method='ordinal')
+        print("after", rankings)
 
     n, ranklength = rankings.shape
 
